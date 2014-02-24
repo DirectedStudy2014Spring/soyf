5b1c2ef2d70df4389d335bda14cf5618d82a2c5c
Alex Wong <alexwong@bu.edu>
2014-02-23 23:11:05 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Soyf::Application.config.secret_key_base = '3b00fa11138177e47678789e93c7fd8a6ed6854b0645347eb5cc515adb58981d35b2ac6cb5de61b3b42caa37d03d42ba5a861ca2199f8e34937a9de06a6ec095'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Soyf::Application.config.session_store :cookie_store, key: '_soyf_session'
